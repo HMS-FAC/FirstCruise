23d7c31298b719aa6eaa4f1c26dad3a0792c0502
Huw <hdrdavies@gmail.com>
2015-11-12 17:46:27 +0000

    add setup files

---
+++ src/js/components/Map.js
@@ -0,0 +1,15 @@
+var React  = require('react');
+
+var Map = React.createClass({
+  render: function () {
+
+    return (
+      <div className="map-container">
+        <h1>Map</h1>
+      </div>
+    )
+
+  }
+});
+
+module.exports = Map;
